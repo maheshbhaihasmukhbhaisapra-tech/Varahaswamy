53da38b85c322cd8746b37d4360dc8a75fb49cce
maheshbhaihasmukhbhaisapra-tech <maheshbhaihasmukhbhaisapra@gmail.com>
2026-02-23 14:03:16 +0530

    Add policy routes to App component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import VarahaswamyLanding from './Home'
+import {
+  BookingPolicy,
+  PrivacyPolicy,
+  TermsConditions,
+  CancellationPolicy
+} from "./PoliciesFiles";
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<VarahaswamyLanding />} />
+        <Route path="/policy/booking" element={<BookingPolicy />} />
+        <Route path="/policy/privacy" element={<PrivacyPolicy />} />
+        <Route path="/policy/terms" element={<TermsConditions />} />
+        <Route path="/policy/cancellation" element={<CancellationPolicy />} />
       </Routes>
     </Router>
   )
